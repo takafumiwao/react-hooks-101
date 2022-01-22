ee7567d369b3b67c5bdde9ef9a32024d622db5fa
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2022-01-22 22:21:35 +0900

    refactor components

---
+++ src/components/EventForm.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+
+const EventForm = ({state, dispatch}) => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const addEvent = e => {
+    e.preventDefault()
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+    setTitle('')
+    setBody('')
+  }
+  const deleteAllEvents = e => {
+    e.preventDefault()
+    const result = window.confirm('全てのイベントを本当に削除しますか?')
+    if (result) dispatch({type: 'DELETE_ALL_EVENTS'})
+  }
+  const unCreatable = title === '' || body === ''
+    return (
+    <>
+    <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control" value={title} onChange={e => setTitle(e.target.value)} id="formEventTitle"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control" value={body} onChange={e => setBody(e.target.value)} id="formEventBody"/>
+        </div>
+        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
+      </form>
+    </>
+    )
+}
+
+
+export default EventForm
+
