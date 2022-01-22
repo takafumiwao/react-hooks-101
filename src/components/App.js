dc0949c45f4b2b83a72cd9b4a15e58a0ef5d58e3
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2022-01-22 21:40:02 +0900

    implement all events deletion feature

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,15 @@ const App =() => {
     setBody('')
   }
 
+  const deleteAllEvents = e => {
+    e.preventDefault()
+    const result = window.confirm('全てのイベントを本当に削除しますか?')
+    if (result) dispatch({type: 'DELETE_ALL_EVENTS'})
+
+  }
+
+  const unCreatable = title === '' || body === ''
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -34,8 +43,8 @@ const App =() => {
           <label htmlFor="formEventBody">ボディー</label>
           <textarea className="form-control" value={body} onChange={e => setBody(e.target.value)} id="formEventBody"/>
         </div>
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
       </form>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
