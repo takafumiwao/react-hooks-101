b84098c38c0300835005a6b659644d160d12a005
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2022-01-23 10:53:32 +0900

    solve prop drilling issue

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 import { DELETE_EVENT } from '../actions'
+import AppContext from '../contexts/AppContext'
 
-const Event = ({event, dispatch}) => {
+const Event = ({event}) => {
     const id = event.id
+    const {dispatch} = useContext(AppContext)
     const handleClickDeleteButton = () => {
         const result = window.confirm(`イベント(id=${id})を削除しますか?`)
         if (result) dispatch({ type: DELETE_EVENT, id})
